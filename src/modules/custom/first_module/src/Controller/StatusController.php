3e18f73d35bcbd28ce35700583a2c6d3f467d279
Liam Hockley <xdega121@gmail.com>
2020-09-30 13:25:02 -0700

    Update module name/path; Create rendered table with content

---
+++ src/modules/custom/first_module/src/Controller/StatusController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\status_module\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class StatusController extends ControllerBase
+{
+  public function content()
+  {
+
+    $intro = 'Welcome to the Task Status Monitor';
+
+    $header = [
+      'Task',
+      'Complete?'
+    ];
+
+    $rows = [
+      [$this->t('Cook Breakfast'), 'X'],
+      [$this->t('Wash Dishes'), 'X'],
+      [$this->t('Get Groceries'), ''],
+      [$this->t('Study Some Drupal'), ''],
+    ];
+
+    $build['introduction'] = [
+      '#markup' => '<p>' . $this->t($intro) . '</p>',
+    ];
+
+    $build['schedule_table'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+      '#empty' => 'No Data Found',
+    ];
+
+    return $build;
+  }
+}
