6b3d1d36bf3df1212c5a8fdb5958676c7b28f7af
Liam Hockley <xdega121@gmail.com>
2020-10-01 10:57:55 -0700

    Add initial form; Clean up module name/namespace

---
+++ src/modules/custom/status_module/src/Form/TaskForm.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\status_module\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class SimpleForm extends FormBase {
+  // Form Content
+}
