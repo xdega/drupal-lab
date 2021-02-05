6feecb8a40e7fef06b9323d74d52c67f53ad9c46
Liam Hockley <xdega121@gmail.com>
2021-02-04 23:36:19 -0800

    Remove Olivero; Clean up install and fix errors.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -10,4 +10,7 @@ $databases['default']['default'] = array (
   'driver' => 'mysql',
 );
 $settings['hash_salt'] = '-rRCQAJk3DzbbgPsesOuULk2MenEVs0XYEhzwoAdI1n-0uryLU4naZGiEYmd_1WDl842U_L6HA';
-$settings['config_sync_directory'] = 'sites/default/files/config_OQyTo7Y1WLmU6mjHZd41hFT5TVpWeXxe8LAfRszFwrrsKqwIthtDFKu0Zpio_j7mxOQPhrLG1w/sync';
+$settings['config_sync_directory'] = 'sites/default/files/config/sync';
+$settings['trusted_host_patterns'] = [
+  '^localhost$',
+];
